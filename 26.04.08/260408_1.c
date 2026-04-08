901dc7755e91d928d08336f7bb2392d21bb1ddda
Reo <pokk123123a@gmail.com>
2026-04-08 15:52:38 +0900

    260408 1교

---
+++ 26.04.08/260408_1.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+void practice(void);
+void practice1(void);
+void practice2(void);
+
+int main(void) {
+	practice();
+	practice1();
+	practice2();
+
+	return 0;
+}
+
+void practice(void) {
+	printf("\n 소 프 트 %d 웨어 %lf %c %s\n", 10, 20.5, 'a', "aaaa");
+}
+
+void practice1(void) {
+	printf("%+5d\n", 12);
+	printf("%07d\n", 14);
+}
+
+void practice2(void) {
+		printf("%d는 16진수로는 %#x, 8진수로는 %#o입니다.\n", 12, 12, 12);
+		//16진수 8진수인지 모르면 샵을 붙여주면됌
+		printf("%7.2lf", 12.34567);
+}		
