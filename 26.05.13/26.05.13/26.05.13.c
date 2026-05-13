712fe692f3caf8fc887031aaa7d87d53698a800e
Reo <pokk123123a@gmail.com>
2026-05-13 15:25:43 +0900

    260513 c언어 수

---
+++ 26.05.13/26.05.13/26.05.13.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+
+
+
+int main(void) {
+
+	return 0;
+}
+
+void example7_1(void){
+	//for (;;) {//무한루프
+	for (int i; i < 10; i++) {
+		printf("Name"); 
+	}
+}
+
+void example7_2(void) {
+	for (int i; i < 10; i++) {
+		printf("%04d : Name\n", i + 1);
+	}
+}
