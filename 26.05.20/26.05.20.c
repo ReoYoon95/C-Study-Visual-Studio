d3e3b30c345b68f0952d4fa233e6d28de3683168
Reo <pokk123123a@gmail.com>
2026-05-20 15:10:49 +0900

    260520 수업시

---
+++ 26.05.20/26.05.20.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+
+
+//void view(void);	//함수 선언 또는 원형
+int example8_1(void)
+{
+	printf("메인 함수 : view호출 전\n");
+	view();		//함수 호출
+	printf("메인 함수 : view호출 후\n");
+	return 0;
+}
+
+void example8_1.1(void)		//함수 정의
+{
+	printf("view함수\n");
+}
