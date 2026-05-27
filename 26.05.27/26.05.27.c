7bf2adb0a75efc64904047199fcc8a6d9728efe5
Reo <pokk123123a@gmail.com>
2026-05-27 16:59:09 +0900

    3교시 시작전

---
+++ 26.05.27/26.05.27.c
@@ -0,0 +1,127 @@
+﻿#include <stdio.h>
+
+int example9_1();
+int add(int x, int y);
+int example9_2(void);
+int example9_3(void);
+void show1(void);
+void show2(void);
+int example9_4(void);
+void sub(void);
+int example9_5(void);
+int example10_1(void);
+
+int main(void)
+{
+	//sub();
+	//sub();
+	//sub();
+	example10_1();
+
+	return 0;
+}
+
+
+int example9_1(void)
+{
+	int x, a = 1, b = 2;
+	x = add(1, 2);//반환값을 x에 대입
+	printf("%d\n", x);
+	printf("%d\n", add(2, 3));
+	x = add(a, b);
+	printf("%d\n", x);
+	return 0;
+}
+
+int add(int x, int y)
+{
+	return x + y;
+}
+
+int example9_2(void)
+{
+	int k = 10;		//지역 변수
+	printf("%d ", k);		//10
+	{
+		int k = 20;		//지역 변수
+		k += 10;
+
+		printf("%d ", k);  //30
+	}
+	k += 5;
+	printf("%d ", k);	//15
+	return 0;
+}
+
+//전역변수는 프로그램 시작이 만들어지고 끝나야지 사라진다.
+//지역변수는 그 블록안에서만 사용되는 것, 작업이 끝나면 사라짐.
+//정적변수는 그 블록안에서만 사용되나, 그 함수를 다시 호출하면 살아남.
+//
+//int example9_3(void)
+//{
+//	//전역 변수
+//	printf("main()에서 x는 %d\n", x);
+//	show1();
+//	printf("show1()호출 후 x는 %d\n", x);
+//	show2();
+//	printf("show2()호출 후 x는 %d\n", x);
+//	return 0;
+//}
+//
+//
+//void show1(void)
+//{
+//	printf("show1()에서 x는 %d\n", ++x);
+//}
+//
+//void show2(void)
+//{
+//	int x = 10;  //지역 변수
+//	printf("show2()에서 x는 %d\n", x);
+//}
+
+int example9_4(void)
+{
+	auto int a = 1;	//여기서 auto는 생략가능
+	{
+		int a = 2;	//auto 변수
+		{
+			int a = 3;	//auto 변수
+			printf("%d ", a);//3
+		}
+		printf("%d ", a);//2
+	}
+	printf("%d ", a);//1
+	return 0;
+}
+
+void sub(void)
+{
+	auto int x = 10;  //auto는 생략가능
+	static int y = 10; //처음 실행시 한번만 초기화
+	printf("x=%d, y=%d\n", x, y);
+	x++;
+	y++;
+}
+
+
+
+int gx = 12, gy = 24;               // 전역변수, 데이터영역에
+int example9_5(void)
+{
+	auto int ax = 15, ay = 25;   // auto변수, 스택에
+	static int sx = 10, sy = 20;  // static변수, 데이터영역에
+	printf("ax=%d 주소=%p, ay=%d 주소=%p\n", ax, &ax, ay, &ay);
+	printf("gx=%d 주소=%p, gy=%d 주소=%p\n", gx, &gx, gy, &gy);
+	printf("sx=%d 주소=%p, sy=%d 주소=%p\n", sx, &sx, sy, &sy);
+	return 0;
+}
+
+int example10_1(void)
+{
+	int x[3] = { 0 };
+	x[2] = 1;
+	printf("%d\n", x[0]);
+
+	return 0;
+}
